ddd4b07a93f863c22fe8022b9d3ec8c8a9ea4fbf
Richard Chou <richardchou@Richard.local>
2025-09-14 08:33:49 +1000

    updated types

--- app/actions/tables/value_extractor.rb
+++ app/actions/tables/value_extractor.rb
@@ -4,6 +4,9 @@ module Tables
   class ValueExtractor
     extend T::Sig
 
+    sig { returns(T::Array[Tables::Column]) }
+    attr_reader :selected
+
     sig { params(selected: T::Array[Tables::Column]).void }
     def initialize(selected:)
       @selected = T.let(selected, T::Array[Tables::Column])
@@ -13,13 +16,18 @@ module Tables
     def record_to_row(record:)
       TableRow.new(
         id: record.id.to_s,
-        cells: @selected.map do |column|
-          Tables::TableCell.new(
-            id: "#{record.id}_#{column.id}",
-            column: column,
-            value: record.public_send(column.id).to_s
-          )
+        cells: selected.map do |column|
+          column_to_cell(column: column, record: record)
         end
+      )
+    end
+
+    sig { params(column: Tables::Column, record: ApplicationRecord).returns(TableCell) }
+    def column_to_cell(column:, record:)
+      Tables::TableCell.new(
+        id: "#{record.id}_#{column.id}",
+        column: column,
+        value: record.public_send(column.id).to_s
       )
     end
   end
--- app/graphql/types/search_result_union.rb
+++ app/graphql/types/search_result_union.rb
@@ -1,21 +1,31 @@
 # typed: strict
 
 module Types
+  class TodoResultType < Types::BaseObject
+    field :id, ID, null: false
+    field :subject, String, null: false
+  end
+
+  class ItemResultType < Types::BaseObject
+    field :id, ID, null: false
+    field :title, String, null: false
+  end
+
   class SearchResultUnion < Types::BaseUnion
     extend T::Sig
 
-    possible_types Types::TodoType, Types::ItemType
+    possible_types TodoResultType, ItemResultType
 
     sig do
       params(object: T.untyped,
-             _ctx: T.untyped).returns(T.any(T.class_of(Types::TodoType), T.class_of(Types::ItemType)))
+             _ctx: T.untyped).returns(T.any(T.class_of(TodoResultType), T.class_of(ItemResultType)))
     end
     def self.resolve_type(object, _ctx)
       case object
       when ::Todo
-        Types::TodoType
+        TodoResultType
       when ::Item
-        Types::ItemType
+        ItemResultType
       else
         raise "Unexpected SearchResult type: #{object.class.name}"
       end

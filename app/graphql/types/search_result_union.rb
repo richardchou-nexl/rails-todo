8a808db60db48678c723de7444073209f8505055
Richard Chou <richardchou@Richard.local>
2025-09-14 15:40:52 +1000

    updated types

--- app/graphql/types/search_result_union.rb
+++ app/graphql/types/search_result_union.rb
@@ -1,21 +1,31 @@
 # typed: strict
 
 module Types
+  class TodoResultType < ::Types::BaseObject
+    field :id, ID, null: false
+    field :subject, String, null: false
+  end
+
+  class ItemResultType < ::Types::BaseObject
+    field :id, ID, null: false
+    field :title, String, null: false
+  end
+
   class SearchResultUnion < Types::BaseUnion
     extend T::Sig
 
-    possible_types TodoType, ItemType
+    possible_types TodoResultType, ItemResultType
 
     sig do
       params(object: T.untyped,
-             _ctx: T.untyped).returns(T.any(T.class_of(TodoType), T.class_of(ItemType)))
+             _ctx: T.untyped).returns(T.any(T.class_of(TodoResultType), T.class_of(ItemResultType)))
     end
     def self.resolve_type(object, _ctx)
       case object
       when ::Todo
-        TodoType
+        TodoResultType
       when ::Item
-        ItemType
+        ItemResultType
       else
         raise "Unexpected SearchResult type: #{object.class.name}"
       end
